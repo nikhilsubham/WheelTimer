03130d1ca74b0864186672c2b277589ae84b7a55
nikhilsubham <nikhilpjain1993@gmail.com>
2019-04-14 15:39:40 +0530

    header file for wheeltimer librar is modified

--- WheelTimer.h
+++ WheelTimer.h
@@ -4,24 +4,25 @@
 #include <pthread.h>
 #include "LinkedListApi.h"
 
+typedef struct _wheeel_timer_slot_event slot_event_elem;
 typedef void (*app_call_back)(void *arg, int sizeof_arg);
 
-typedef struct _wheeel_timer_slot_event{
+struct _wheeel_timer_slot_event{
      int time_interval;
      int execution_cycle_no;
-     int is_recurrence;
      app_call_back app_callback;
      void* args;
      int size_of_args;
-}slot_event_elem;
+     int is_recurrence;
+};
 
 typedef struct _wheel_timer_t{
      int wheel_needle_interval;
-     int current_wheel_needle;
      int wheel_size;
+     int current_wheel_needle;
      int current_cycle_no;
-     ll_t* slots[0];
      pthread_t wheel_thread; 
+     ll_t* slots[0];
 }wheel_timer_t;
 
 wheel_timer_t* init_wheel_timer(int clock_needle_interval, int wheel_size);
@@ -34,7 +35,7 @@ slot_event_elem* register_app_event(wheel_timer_t* wt, app_call_back app_callbac
 
 void start_wheel_timer(wheel_timer_t *wt);
 
-static void* wheel_fn(void* arg);
+//static void* wheel_fn(void* arg);
 
 void free_wheel_timer_element(slot_event_elem *elem);
 
